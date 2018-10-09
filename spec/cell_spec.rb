2fd8a1c9b232e48baf389c268939527f94bd285d
Christopher Manahan <christophermanahan@Christophers-MacBook-Pro.local>
2018-10-09 10:51:21 -0400

    initialize project, add cell tests

---
+++ spec/cell_spec.rb
@@ -0,0 +1,23 @@
+require "./lib/cell.rb"
+
+describe "cell" do
+	let (:cell) { cell = Cell.new }
+
+	it "Is initialized with symbol set to nil" do
+		expect(cell.symbol).to eq nil
+	end
+
+	it "Is initialized with empty? set to true" do
+		expect(cell.empty?).to eq true
+	end
+
+	it "Sets the symbol to the provided value" do
+		cell.symbol = "X"
+		expect(cell.symbol).to eq "X"
+	end
+
+	it "Returns false from empty? after a symbol is set" do
+		cell.symbol = "X"
+		expect(cell.empty?).to eq false
+	end
+end
