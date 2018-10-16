d2ed3241280e6e18af1c27e00d807bba263380c6
Christopher Manahan <chris.a.manahan@gmail.com>
2018-10-16 16:14:14 -0400

    Added main

---
+++ lib/main.rb
@@ -0,0 +1,21 @@
+require "./lib/cell"
+require "./lib/board"
+require "./lib/board_formatter"
+require "./lib/displayer"
+require "./lib/input"
+require "./lib/game"
+
+class Main
+	def start(io)
+		cells = Array.new(9) do
+			Cell.new
+		end
+		board = Board.new(cells)
+		board_formatter = BoardFormatter.new
+		symbols = ["X", "O"]
+		displayer = Displayer.new(io)
+		input = Input.new(io)
+		game = Game.new(board, board_formatter, symbols, displayer, input)
+		game.run
+	end
+end
