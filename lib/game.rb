c4a2fa8fedb97ab2221246214653d8eaf46e2abd
Christopher Manahan <chris.a.manahan@gmail.com>
2018-10-16 16:13:42 -0400

    Added game

---
+++ lib/game.rb
@@ -0,0 +1,32 @@
+class Game
+	def initialize(board, board_formatter, symbols, displayer, input)
+		@board = board
+		@board_formatter = board_formatter
+		@symbols = symbols
+		@displayer = displayer
+		@input = input
+	end
+
+	def run()
+		display_board
+		if !board.full?
+			move(symbols.first)
+			@symbols = symbols.reverse
+			run
+		end
+	end
+
+	private
+	attr_reader :board, :board_formatter, :symbols, :displayer, :input
+	def display_board()
+		board_array = board.get_board
+		formatted_board = board_formatter.format_board(board_array)
+		displayer.display(formatted_board)
+	end
+
+	def move(symbol)
+		displayer.display("Choose a Move! (1-9)")
+		position = input.get
+		board.set_symbol(symbol, position)
+	end
+end
