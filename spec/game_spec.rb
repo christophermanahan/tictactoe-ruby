70f30923d90aa4b766cf7c22d401f11f3b116afe
Christopher Manahan <chris.a.manahan@gmail.com>
2018-10-18 11:04:08 -0400

    Reformat game spec args

--- spec/game_spec.rb
+++ spec/game_spec.rb
@@ -52,19 +52,37 @@ describe "game" do
   end
 
   it "Displays the formatted board" do
-    game = Game.new(board: MockBoard.new(0), board_formatter: MockBoardFormatter.new, displayer: displayer, input: MockInput.new, symbols: symbols)
+    game = Game.new(
+      board: MockBoard.new(0),
+      board_formatter: MockBoardFormatter.new,
+      displayer: displayer,
+      input: MockInput.new,
+      symbols: symbols
+    )
     game.run
     expect(displayer.log).to eq "|[]|"
   end
 
   it "Puts the current player's move on the board if it is not full" do
-    game = Game.new(board: MockBoard.new(1), board_formatter: MockBoardFormatter.new, displayer: displayer, input: MockInput.new, symbols: symbols)
+    game = Game.new(
+      board: MockBoard.new(0),
+      board_formatter: MockBoardFormatter.new,
+      displayer: displayer,
+      input: MockInput.new,
+      symbols: symbols
+    )
     game.run
     expect(displayer.log).to eq "|[X 1]|"
   end
 
   it "Puts the next player's move on the board if it is still not full" do
-    game = Game.new(board: MockBoard.new(2), board_formatter: MockBoardFormatter.new, displayer: displayer, input: MockInput.new, symbols: symbols)
+    game = Game.new(
+      board: MockBoard.new(0),
+      board_formatter: MockBoardFormatter.new,
+      displayer: displayer,
+      input: MockInput.new,
+      symbols: symbols
+    )
     game.run
     expect(displayer.log).to eq "|[O 1]|"
   end
