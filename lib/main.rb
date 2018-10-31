5e262a118e914bc467669c9b6ec95be2eb93d7ef
Christopher Manahan <chris.a.manahan@gmail.com>
2018-10-31 18:00:58 -0400

    Refactor main to use colorizer

--- lib/main.rb
+++ lib/main.rb
@@ -3,6 +3,7 @@ require './lib/displayer'
 require './lib/messages'
 require './lib/board'
 require './lib/input'
+require './lib/colorizer'
 require './lib/formatter'
 require './lib/presenter'
 require './lib/game'
@@ -11,10 +12,11 @@ class Main
   def start(io, clear_console, default_board_size)
     cells = Array.new(default_board_size**2) { Cell.new }
     symbols = %w[O X].cycle
+    formatter = Formatter.new(Colorizer.new)
     presenter = Presenter.new(
       displayer: Displayer.new(io),
       clear: clear_console,
-      formatter: Formatter.new
+      formatter: formatter
     )
     Game.new(
       board: Board.new(cells, default_board_size),
