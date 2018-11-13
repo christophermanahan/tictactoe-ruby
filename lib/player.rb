da44ccef0cc2c8827d77d1b06318423efa758c99
Christopher Manahan <chris.a.manahan@gmail.com>
2018-11-13 15:18:15 -0500

    Refactor player to use new game class

--- lib/player.rb
+++ lib/player.rb
@@ -1,18 +1,16 @@
 class Player
   attr_reader :symbol, :move_message, :win_message
 
-  def initialize(move_maker:, symbol:, move_message:, win_message:)
-    @move_maker = move_maker
+  def initialize(move:, symbol:)
+    @move = move
     @symbol = symbol
-    @move_message = move_message
-    @win_message = win_message
   end
 
-  def make_move
-    move_maker.get
+  def play(game)
+    game.move(symbol: symbol, to: move.get)
   end
 
   private
 
-  attr_reader :move_maker
+  attr_reader :move
 end
--- spec/player_spec.rb
+++ spec/player_spec.rb
@@ -1,38 +1,24 @@
 require './lib/player'
 
 describe 'player' do
-  class StubMoveMaker
+  class StubMove
     def get
       '1'
     end
   end
 
-  let(:move_message) { 'move' }
+  class StubGame
+    def move(symbol:, to:)
+      symbol + to
+    end
+  end
 
-  let(:win_message) { 'win' }
-
-  let(:player) do
-    Player.new(
-      move_maker: StubMoveMaker.new,
-      symbol: 'X',
-      move_message: move_message,
-      win_message: win_message
+  it 'play a turn of the game' do
+    player = Player.new(
+      move: StubMove.new,
+      symbol: 'X'
     )
-  end
-
-  it 'can make a move' do
-    expect(player.make_move).to eq '1'
-  end
-
-  it 'has a symbol' do
-    expect(player.symbol).to eq 'X'
-  end
-
-  it 'has a move message' do
-    expect(player.move_message).to eq move_message
-  end
-
-  it 'has a win message' do
-    expect(player.win_message).to eq win_message
+    game = StubGame.new
+    expect(player.play(game)).to eq 'X1'
   end
 end
