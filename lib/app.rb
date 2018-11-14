88a21e9eb22e68ba39cb3394ffd1db739f328da7
Christopher Manahan <chris.a.manahan@gmail.com>
2018-11-14 14:38:25 -0500

    Refactor app to start updated main

--- lib/app.rb
+++ lib/app.rb
@@ -4,14 +4,10 @@ OPEN_READ_WRITE = 1
 CLEAR_CONSOLE = "\e[H\e[2J".freeze
 DEFAULT_BOARD_SIZE = 3
 DEFAULT_SYMBOLS = %w[O X].freeze
-DEFAULT_MOVE_MESSAGE_PREFIX = 'Please make a selection for '.freeze
-DEFAULT_WIN_MESSAGE_SUFFIX = ' has won!'.freeze
 
 Main.new.start(
   io: IO.new(OPEN_READ_WRITE),
   clear_console: CLEAR_CONSOLE,
   default_board_size: DEFAULT_BOARD_SIZE,
-  default_symbols: DEFAULT_SYMBOLS,
-  default_move_message_prefix: DEFAULT_MOVE_MESSAGE_PREFIX,
-  default_win_message_suffix: DEFAULT_WIN_MESSAGE_SUFFIX
+  default_symbols: DEFAULT_SYMBOLS
 )
